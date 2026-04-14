6508b7edd99b5ffd894042f7960a6feea7bb65c6
sudip dhungana <dhunganasudip56@gmail.com>
2026-04-14 16:45:39 +0545

    fix API base URL

--- client/src/api.js
+++ client/src/api.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
 
-const API_BASE = process.env.REACT_APP_API_URL || 'http://localhost:5000/api';
+const API_BASE =
+  process.env.REACT_APP_API_URL ||
+  'https://sudip-portfoili.onrender.com/api';
 
 const api = axios.create({
   baseURL: API_BASE,
