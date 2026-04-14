887dabed822dc20534ba8105204771dbec3c89ce
sudip dhungana <dhunganasudip56@gmail.com>
2026-04-14 11:07:16 +0545

    fix cors

--- server/server.js
+++ server/server.js
@@ -7,8 +7,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: process.env.CLIENT_URL || 'http://localhost:3000',
-  credentials: true
+  origin: "*"
 }));
 app.use(express.json());
 
